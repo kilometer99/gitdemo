5c989773723371b08921c225f51222ecd682aab8
kilometer <kongmo123@sina.cn>
2020-04-01 18:57:26 +0800

    update homework.py

--- homework.py
+++ homework.py
@@ -8,3 +8,17 @@
 #大学计算机基础实验6练习题1
 def f(i,j,n):
     L=[[0 for col in range(n)]for raw in range(n)] #创建二维列表
+    for i in range(0,n):
+        if i%2==0:
+            L[i][0]=1
+        else:
+            L[i][0]=2
+    for j in range(0,n):
+        if j%2==0:
+            L[0][j]=1
+        else:
+            L[0][j]=2
+    for i in range(1,n):
+        for j in range(1,n):
+            L[i][j]=L[i-1][j]+L[i][j-1]
+    return L[i-1][j-1]  #运用动态规划
